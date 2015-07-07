bb4e4ccbc868589b9cf89cb305d9f7910c91d09d
HenryEdmodo <henry@edmodo.com>
2015-07-07 16:38:06 -0700

    add js file
    added

---
+++ rent.js
@@ -0,0 +1,54 @@
+var thisMonth = "";
+
+$( document ).ready(function() {
+  $("#rent-button-submit").hide();
+  $('#rent-button-month').click(function() {
+    thisMonth = $("#the-month").val();  
+    $("#rent-button-submit").show(300);
+  });
+});
+
+// send rent charges to roommates on the 23rd`
+
+  // SHARED DATA  
+  
+  var workedForPerson = function(data) {
+    var resultWorked = name + " has been charged rent for " + thisMonth + ".";
+    $('#results').append("<li class='list-group-item list-group-item-success'>" + resultWorked + "</li>");
+    alert(data);
+  }
+  /*
+  var didNotWorkForPerson = function(name) {
+    var result = name + " was not charged. Error occurred.";
+    $('#results').append("<li>" + result + "</li>");
+  }
+  */
+
+  $('#rent-button-submit').click(function(){        
+    // UNCOMMENT NEXT LINE ONLY WHEN READY TO CHARGE RENT
+    chargeEveryone();   
+    //chargeEveryone().done(workedForPerson);
+  });
+
+  var chargeEveryone = function() {     
+    var theResponse;  
+    for (i = 0; i < roommatesData.length; i++) {
+      roommatesData[i].apiData.note = "I am testing my app for hack reactor interview does this show the month? " + thisMonth;             
+    }
+
+    for (i = 0; i < roommatesData.length; i++) {      
+      return $.ajax({
+        type: "POST",
+        url: 'https://api.venmo.com/v1/payments',
+        dataType: "json",
+        data: roommatesData[i].apiData,
+        contentType: "application/x-www-form-urlencoded",  
+        success: workedForPerson,      
+        // success: workedForPerson(roommatesData[i].name),
+        // success: workedForPerson
+        crossDomain: true
+        // failure: didNotWorkForPerson(roommatesData[i].name)
+      })      
+    }   
+  }
+
